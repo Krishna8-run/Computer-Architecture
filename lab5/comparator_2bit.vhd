90216e2d3c7f14de4599b873fcd38cf105c08418
https://github.com/Raj-tec551 <crpanta8@gmail.com>
2026-06-24 14:50:09 +0530

    updated

--- lab5/comparator_2bit.vhd
+++ lab5/comparator_2bit.vhd
@@ -16,12 +16,8 @@ architecture Behavioral of COMPARATOR_2BIT is
 begin
     process(A, B)
     begin
-        if unsigned(A) = unsigned(B) then
-            EQ <= '1'; GT <= '0'; LT <= '0';
-        elsif unsigned(A) > unsigned(B) then
-            EQ <= '0'; GT <= '1'; LT <= '0';
-        else
-            EQ <= '0'; GT <= '0'; LT <= '1';
-        end if;
+        EQ <= (A(1) xnor B(1)) and (A(0) xnor B(0)); -- A = B
+        GT <= (A(1) and not B(1)) or ((A(1) xnor B(1)) and A(0) and not B(0));
+        LT <= (not A(1) and B(1)) or ((A(1) xnor B(1)) and not A(0) and B(0));
     end process;
-end architecture Behavioral;
+    end architecture Behavioral;
